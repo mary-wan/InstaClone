1ec29b9392b857e0f153d81d345f24ff99b1a4a7
mary-wan <mary.njenga@student.moringaschool.com>
2021-12-05 18:36:46 +0300

    add display comments

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.conf.urls import url
+from django.urls.conf import re_path
 from . import views
 
 urlpatterns = [
     path('',views.index, name='home'),
-    url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
-    # url(r"^comment/(\d+)", views.comment, name="comment"),
+    # url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+    # re_path(r"^post/(\d+)", views.comment, name="comment"),
     url(r"^like/(\d+)", views.like, name="like"),
-    url(r"^post/(\d+)", views.post, name="post"),
+    # url(r"^post/(\d+)", views.post_comment, name="comment"),
+    path('post/<id>', views.comment, name='comment'),
     path('profile/',views.profile, name='profile'),
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -45,23 +45,23 @@ def index(request):
 
 
 
-@login_required(login_url='/accounts/login/')
-def comment(request,image_id):
-    #Getting comment form data
-    image = get_object_or_404(Image, pk=image_id)
-    current_user =request.user
-    comments =Comments.get_comments_by_images(image_id).all()
-    # comments = Comments.objects.filter(image__id=image_id)
-    if request.method == 'POST':
-        image = get_object_or_404(Image, pk = image_id)
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.user = current_user
-            comment.image = image
-            comment.save()
-        # return redirect('home')
-    return render(request,'all-instagram/post.html',{'image':image,'comments':comments,'form':form})
+# @login_required(login_url='/accounts/login/')
+# def comment(request,image_id):
+
+#     image = get_object_or_404(Image, pk=image_id)
+#     current_user =request.user
+#     comments =Comments.get_comments_by_images(image_id).all()
+
+#     if request.method == 'POST':
+#         image = get_object_or_404(Image, pk = image_id)
+#         form = CommentForm(request.POST)
+#         if form.is_valid():
+#             comment = form.save(commit=False)
+#             comment.user = current_user
+#             comment.image = image
+#             comment.save()
+
+#     return render(request,'all-instagram/post.html',{'image':image,'comments':comments,'form':form})
 
 def like(request, id):
     post = Image.objects.get(id = id)
@@ -69,30 +69,47 @@ def like(request, id):
     post.save()
     return HttpResponseRedirect(reverse("home"))
 
+  
 
-    
+# def post(request,image_id):
+#     post = Image.objects.get(id = image_id)
+#     comments = Comments.objects.filter(image__id=image_id)
+#     current_user = request.user
 
-def post(request,image_id):
-    post = Image.objects.get(id = image_id)
-    comments = Comments.objects.filter(image__id=image_id)
-    current_user = request.user
-    # current_profile = Profile.objects.get(post=id)
 
-    if request.method == "POST":
-        comment_form = CommentForm(request.POST)
+#     if request.method == "POST":
+#         comment_form = CommentForm(request.POST)
 
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.user = current_user
-            comment.post = post
-            comment.save()
-            comment_form = CommentForm()
-            return redirect("post", post.image_id)
+#         if comment_form.is_valid():
+#             comment = comment_form.save(commit=False)
+#             comment.user = current_user
+#             comment.post = post
+#             comment.save()
+#             comment_form = CommentForm()
+#             return redirect("post", post.image_id)
 
-    else:
-        comment_form = CommentForm()
+#     else:
+#         comment_form = CommentForm()
 
-    return render(request, "all-instagram/post.html")
+#     return render(request, "all-instagram/post.html")
 
 def profile(request):
     return render(request,"all-instagram/profile.html")
+
+@login_required(login_url='login')
+def comment(request, id):
+    image = get_object_or_404(Image, pk=id)
+ 
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            savecomment = form.save(commit=False)
+            savecomment.post = image
+            savecomment.user = request.user
+            savecomment.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = CommentForm()
+
+    return render(request, 'all-instagram/post.html', {'post': image,'form': form,})
+
