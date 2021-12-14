7e228c09372476fe2463988f3c9d9ccb42406693
mary-wan <mary.njenga@student.moringaschool.com>
2021-12-14 04:32:53 +0300

    key

--- InstaClone/urls.py
+++ InstaClone/urls.py
@@ -26,6 +26,13 @@ urlpatterns = [
     path('accounts/login/',auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/',auth_views.LogoutView.as_view(), name='logout'),
     path('social-auth/',include('social_django.urls',namespace='social')),
+    # path('password-reset/',auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'),
+    #      name='password_reset'),
+    # path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+    #      name='password_reset_done'),
+    # path('password-reset-confirm/<uidb64>/<token',auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'),
+    #      name='password_reset_confirm'),
+    
 
 
     
